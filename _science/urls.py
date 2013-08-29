4b5804ff3a041059ef66765d13882e675787b644
Ian Hung <ianmenghung@gmail.com>
2013-08-29 07:24:42 -0400

    ec2

--- _science/urls.py
+++ _science/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
 
-# Uncomment the next two lines to enable the admin:
+# Uncomment the next two lines to enable the admin: will this show
 
 from django.contrib import admin
 admin.autodiscover()
