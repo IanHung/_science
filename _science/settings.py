8c3f4ea2b98adc9c693977af49e9658277f40882
Ian Hung <ianmenghung@gmail.com>
2013-08-30 04:07:01 -0400

    settings.py issues.

--- _science/settings.py
+++ _science/settings.py
@@ -54,7 +54,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = os.path.join(PROJECT_DIR, 'media')
+MEDIA_ROOT = os.path.join(PROJECT_DIR, 'media').replace('\\','/')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -76,7 +76,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-	os.path.join(PROJECT_DIR, 'static'),
+	os.path.join(PROJECT_DIR, 'static').replace('\\','/'),
 	
 )
 
@@ -117,7 +117,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-	os.path.join(PROJECT_DIR, 'templates'),
+	os.path.join(PROJECT_DIR, 'templates').replace('\\','/'),
 )
 
 INSTALLED_APPS = (
