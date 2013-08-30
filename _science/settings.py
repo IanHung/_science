f0f481c388fc1102653fcb2f115734e12f476bb9
Ian Hung <ianmenghung@gmail.com>
2013-08-30 13:04:01 -0400

    debug false.

--- _science/settings.py
+++ _science/settings.py
@@ -4,7 +4,7 @@ from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS as TCP
 PROJECT_DIR = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..'))
 # Django settings for _science project.
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
