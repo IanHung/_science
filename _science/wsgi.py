9a9eaa4ff89626285e874138e115cd235302f1c3
Ian Hung <ianmenghung@gmail.com>
2013-09-03 16:50:20 -0400

    testing

--- _science/wsgi.py
+++ _science/wsgi.py
@@ -30,7 +30,7 @@ _application = get_wsgi_application()
 
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
-# application = HelloWorldApplication(application) TESTI
+# application = HelloWorldApplication(application) TESTING
 
 #adding apache environ variables to os.environ
 DEBUG = False
