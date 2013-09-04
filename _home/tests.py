84ba09aba5ec65fb1d641075ae43a4e4830e803b
Ian Hung <ianmenghung@gmail.com>
2013-09-04 13:42:02 -0400

    Added firsrt unittest.

--- _home/tests.py
+++ _home/tests.py
@@ -6,7 +6,9 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
-
+from selenium import webdriver
+from _home.views import home
+from django.core.urlresolvers import resolve
 
 class SimpleTest(TestCase):
     def test_basic_addition(self):
@@ -14,3 +16,13 @@ class SimpleTest(TestCase):
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+        
+class HomePageTest(TestCase):
+    
+    #Can we resolve the URL /
+    # Can we make this view return the desired html content
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home)
+
+    
